df7b68be2b2d9eb4960f322ebfdef7a20e00f801
Hussain Ghazali <hussainghazali@gmail.com>
2021-09-07 12:22:21 +0500

    new changes

---
+++ AngularApp/src/app/pre-login/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  public slides
+  constructor() {}
+
+  ngOnInit(): void {
+    this.slides = [
+      {
+        title: 'summer sale',
+        subTitle: 'Phone',
+        image: "assets/images/bg_1.jpg"
+      },
+      {
+        image: "assets/images/bg_2.jpg"
+      },
+      {
+        image: "assets/images/bg_3.jpg"
+      },
+      {
+        image: "assets/images/bg_4.jpg"
+      }
+    ]
+
+
+  }
+}
